fdf7634fb462a6c8262a82b53518ac7caa0e71b9
DmitriyKaplan28 <dsk007@mail.ru>
2022-06-26 20:58:21 +0300

    after lesson#13 updated

--- src/stories/tasks-api.stories.tsx
+++ src/stories/tasks-api.stories.tsx
@@ -57,46 +57,5 @@ export const UpdateTask = () => {
     return <div> {JSON.stringify(state)}</div>
 }
 
-/*
-export const CreateTodolist = () => {
-    const [state, setState] = useState<any>(null)
-    useEffect(() => {
-        todolistAPI.createTodolist( 'NEW_AZAZA')
-            .then( (res) => {
-            setState(res.data);
-        } )
-
-    }, [])
-
-    return <div> {JSON.stringify(state)}</div>
-}
-export const DeleteTodolist = () => {
-    const [state, setState] = useState<any>(null)
-    useEffect(() => {
-        const todolistId = 'c5b648ba-08a3-4002-adc7-3a0516f6ca8b';
-        todolistAPI.deleteTodolist(todolistId)
-            .then( (res) => {
-            setState(res.data);
-        })
-
-    }, [])
-
-    return <div> {JSON.stringify(state)}</div>
-}
-
-
-export const UpdateTodolistTitle = () => {
-    const [state, setState] = useState<any>(null)
-    useEffect(() => {
-        const todolistId = 'c5b648ba-08a3-4002-adc7-3a0516f6ca8b'
-        todolistAPI.updateTodolist(todolistId, 'AZAZA')
-            .then((res) => {
-                setState(res.data)
-            })
-    }, [])
-
-    return <div> {JSON.stringify(state)}</div>
-}
-*/
 
 
