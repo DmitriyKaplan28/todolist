130f91738939c75f4fb5756ec55e59d6f666b399
DmitriyKaplan28 <dsk007@mail.ru>
2022-08-04 17:26:16 +0300

    16 Authorization flow 19 is done+

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -17,25 +17,28 @@ export const Login = () => {
     const isLoggedIn = useSelector((state: AppRootStateType) => state.auth.isLoggedIn)
 
     const formik = useFormik({
+
         initialValues: {
             email: '',
             password: '',
             rememberMe: false
         },
+
         validate: (values) => {
             const errors: FormikErrorType = {};
             if (!values.email) {
                 errors.email = 'Required';
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
                 errors.email = 'Invalid email address';
-            };
+            }
             if (!values.password) {
                 errors.password = 'Required';
             } else if (values.password.length < 3) {
                 errors.password = 'Invalid password';
-            };
+            }
             return errors;
         },
+
         onSubmit: values => {
             alert(JSON.stringify(values));
             formik.resetForm();
