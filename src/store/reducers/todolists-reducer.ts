01f7b096cdc55dfa17cab4f1ab6c5cb1425524eb
DmitriyKaplan28 <dsk007@mail.ru>
2022-09-07 14:47:56 +0300

    17 delete: comments in todolist reducer

--- src/store/reducers/todolists-reducer.ts
+++ src/store/reducers/todolists-reducer.ts
@@ -48,45 +48,6 @@ export const {
 } = slice.actions
 
 export const todolistsReducer = slice.reducer
-/*(todolists
-:
-Array < TodolistType > = initialTodolistsState, action
-:
-TodolistActionsType
-):
-Array < TodolistType >
-=>
-{
-    switch (action.type) {
-        case "REMOVE-TODOLIST":
-            return todolists.filter(tl => tl.id !== action.id)
-        case 'ADD-TODOLIST':
-            return [{...action.todolist, filter: 'all', entityStatus: 'idle'}, ...todolists]
-        case "CHANGE-TODOLIST-FILTER":
-            return todolists.map(tl => tl.id === action.id ? {...tl, filter: action.value} : tl)
-        case "CHANGE-TODOLIST-TITLE":
-            return todolists.map(tl => tl.id === action.id ? {...tl, title: action.title} : tl)
-        case "CHANGE-TODOLIST-ENTITY-STATUS":
-            return todolists.map(tl => tl.id === action.id ? {...tl, entityStatus: action.entityStatus} : tl)
-        case 'SET-TODOLISTS':
-            return action.todolists.map(tl => ({...tl, filter: 'all', entityStatus: 'idle'}))
-        default:
-            return todolists
-    }
-}*/
-
-//actions
-/*export const removeTodolistAC = (id: string) =>
-    ({type: 'REMOVE-TODOLIST', id} as const)
-export const addTodolistAC = (todolist: TodolistAPIType) => ({type: 'ADD-TODOLIST', todolist} as const)
-export const changeTodolistFilterAC = (id: string, value: FilterValuesType) =>
-    ({type: 'CHANGE-TODOLIST-FILTER', id, value} as const)
-export const changeTodolistTitleAC = (id: string, title: string) =>
-    ({type: 'CHANGE-TODOLIST-TITLE', id, title} as const)
-export const setTodolistsAC = (todolists: TodolistAPIType[]) =>
-    ({type: 'SET-TODOLISTS', todolists} as const)
-export const changeTodolistEntityStatusAC = (id: string, entityStatus: RequestStatusType) =>
-    ({type: 'CHANGE-TODOLIST-ENTITY-STATUS', id, entityStatus} as const)*/
 
 //thunks
 export const fetchTodolistsTC = () => (dispatch: Dispatch) => {
@@ -161,13 +122,3 @@ export type TodolistType = TodolistAPIType & {
     filter: FilterValuesType,
     entityStatus: RequestStatusType
 }
-/*export type RemoveTodoListAT = ReturnType<typeof removeTodolistAC>
-export type AddTodoListAT = ReturnType<typeof addTodolistAC>
-export type SetTodolistsAT = ReturnType<typeof setTodolistsAC>*/
-/*export type TodolistActionsType = RemoveTodoListAT
-    | AddTodoListAT
-    | SetTodolistsAT
-    | ReturnType<typeof changeTodolistFilterAC>
-    | ReturnType<typeof changeTodolistTitleAC>
-    | ReturnType<typeof setTodolistsAC>
-    | ReturnType<typeof changeTodolistEntityStatusAC>*/
