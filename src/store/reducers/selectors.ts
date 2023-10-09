3aa6bab3b695151bacec77c693f591208f0ceccf
DmitriyKaplan28 <dsk007@mail.ru>
2023-10-09 17:31:53 +0300

    check

--- src/store/reducers/selectors.ts
+++ src/store/reducers/selectors.ts
@@ -1,5 +1,6 @@
 import {AppRootStateType} from "../store";
 
+
 export const selectStatus = (state: AppRootStateType) => state.app.status;
 export const selectIsInitialized = (state: AppRootStateType) => state.app.isInitialized;
 export const selectIsLoggedIn = (state: AppRootStateType) => state.auth.isLoggedIn;
